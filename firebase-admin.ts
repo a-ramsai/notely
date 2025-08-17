1738f8e25676a51999e7ffc326cfcb6594d0f727
a-ramsai <a.ramsai24j@gmail.com>
2025-08-17 15:17:11 +0530

    2nd

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,7 +2,14 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore"; 
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const serviceKey = require("./service_key.json");
+const serviceKey = {
+  apiKey: "AIzaSyAuhWgdlJXNWRqw9JpDeHi6OfvbGGRLcns",
+  authDomain: "notely-608b0.firebaseapp.com",
+  projectId: "notely-608b0",
+  storageBucket: "notely-608b0.firebasestorage.app",
+  messagingSenderId: "958367213264",
+  appId: "1:958367213264:web:f571285cf6752a226a20d5"
+};
 
 let app: App;
 if (getApps().length === 0) {
